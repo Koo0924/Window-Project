c1abe749e7132366d3391f0a6df5488c5679a9de
Koo Sangho <kshh0924@naver.com>
2024-12-08 15:59:36 +0900

    add Brush
    Form1 Color Lime

---
+++ Brush/Brush/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace Brush
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            SolidBrush b = new SolidBrush(Color.Lime);
+            g.FillRectangle(b, ClientRectangle);
+        }
+    }
+
+   
+
+}
